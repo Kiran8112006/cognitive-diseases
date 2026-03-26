feba0a01e46eeaf7115c2c37d9a4e9053360d418
tanish-211 <tanishrm2005@gmail.com>
2026-03-26 16:41:35 +0530

    LANDING

--- bmsce/src/landing.jsx
+++ bmsce/src/landing.jsx
@@ -1,12 +1,74 @@
-export default function Landing({ goToAuth }) {
+﻿export default function Landing({ goToAuth })
+ {
     return (
         <div style={{ textAlign: "center", marginTop: "100px" }}>
-            <h1>Welcome to this world</h1>
-            <p>Convert speech → text → syllables → mindmaps</p>
-
+            <h1>DDAP</h1>
             <button onClick={goToAuth}>
                 Login / Signup
             </button>
-        </div>
+                <h3>CONGINITIVE DISEASES</h3>
+                <p>Cognitive diseases are disorders that affect a person’s ability to think, 
+                    remember, learn, and make decisions. They primarily involve damage or 
+                    dysfunction in the brain, leading to a decline in mental abilities over 
+                    time. Common examples include Alzheimer’s disease, dementia, Parkinson’s 
+                    disease, and other neurodegenerative conditions. These diseases can result 
+                    from factors such as aging, genetic predisposition, brain injury, or 
+                    underlying medical conditions. Symptoms often begin subtly, such as mild 
+                    memory loss or difficulty concentrating, but may progress to severe 
+                    impairment in daily functioning, communication, and independence. 
+                    While many cognitive diseases currently have no cure, early diagnosis, 
+                    proper treatment, and supportive care can help manage symptoms and improve 
+                    quality of life.</p>
+                <h3>About Problem statement</h3>
+                <p> blah blahaahahaabhjdawdabjdad</p>
+                <h3>Dyslexia</h3>
+                <p>Dyslexia is a learning disorder that affects a person’s ability to read, 
+                    spell, and process written language, even though their intelligence is 
+                    usually normal. It occurs due to differences in how the brain processes 
+                    language, making it difficult to recognize words and connect letters to 
+                    sounds. People with dyslexia may read slowly, confuse similar letters, or 
+                    struggle with spelling, but they often have strong creative and 
+                    problem-solving skills. With the right support, such as specialized 
+                    teaching methods and practice, individuals with dyslexia can improve their
+                     reading abilities and succeed academically.</p>
+                     
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_3wys4e1z3zHSjP0Zfs0Oik-lJycvN5HZzA&amp;s"></img>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQR7QOTYA4fuhtXSNFCCwxnShjWnCzxhBmiHw&amp;s"></img>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSjGcpaXg7saJP-TfiZ52e9iiZY5tnNxFeLg&amp;s"></img>
+                <img src="https://cdn.sanity.io/images/68lp9qid/production/cc0eaf9999c60ae95448087534dd9e9eda663d93-1200x750.jpg/27-june-2022-Dyslexia-editoriall.jpg?rect=37,0,1127,750&w=320&h=213&fit=min&auto=format"></img>
+                <h3>Dyscalculia</h3>
+                <p>Dyscalculia is a learning disorder that affects a person’s ability to 
+                    understand and work with numbers. It makes it difficult to grasp basic 
+                    math concepts, such as counting, number recognition, and arithmetic. 
+                    People with dyscalculia may struggle with telling time, managing money, 
+                    or solving math problems, even if they are otherwise intelligent. 
+                    This condition is not related to intelligence or motivation, but rather 
+                    to differences in how the brain processes numerical information. 
+                    With appropriate support, such as specialized teaching strategies and 
+                    practice, individuals with dyscalculia can develop math skills and 
+                    succeed in school and daily life.</p>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR10hoJTeDUqSjj58gbM6olS3mllTg_AsLRpg&s"></img>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROKBN3Q9aMQ7ZvgyMz7_TMNsbfKh5yYiTi0w&s"></img>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUNU4oVMwhoyTqFiRe0Pw2VgkEq4aD7gWVdA&s"></img>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzcRrFpcMS3vTTt6mKheN34wcgZCG18os2fg&s"></img>
+                <h3>Adenosine Diphosphate</h3>
+                <p>Adenosine Diphosphate (ADP) is a molecule that plays a crucial role in cellular 
+                    energy transfer. It is composed of adenosine, which consists of an adenine base 
+                    attached to a ribose sugar, and two phosphate groups. ADP is formed when ATP 
+                    (Adenosine Triphosphate), the primary energy carrier in cells, loses one of 
+                    its phosphate groups during energy release. This process is essential for various 
+                    cellular functions, including muscle contraction, nerve impulse transmission,
+                     and biochemical reactions. ADP can be converted back into ATP through cellular 
+                     respiration, allowing cells to maintain their energy balance and perform necessary 
+                     activities.</p>
+                <img src="https://institute.careerguide.com/wp-content/uploads/2023/10/adp-full-form.png"></img>
+                <img src="https://i.ytimg.com/vi/tl0oPr97e8U/hq720.jpg?sqp=-oaymwE7CK4FEIIDSFryq4qpAy0IARUAAAAAGAElAADIQj0AgKJD8AEB-AH-CYAC0AWKAgwIABABGCkgWChyMA8=&rs=AOn4CLC9gEfRSjd-f94pmNKkT6zLSuSGvg"></img>
+                <img src="https://study.com/cimages/multimages/16/39_image_01_adp_structure_4509084119542572263113.jpg"></img>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkHeGZniS8bxYDzM8rslwEI5EXJBPgkcWipA&s"></img>
+                <h6>Connect with us</h6>
+                <a href="https://github.com/Tanish-2112"><img src="https://upload.wikimedia.org/wikipedia/commons/c/c2/GitHub_Invertocat_Logo.svg" alt="Tanish" width="30" height="30"></img></a>
+                <a href="https://www.instagram.com/_heyy_sxmarth/#"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/CIS-A2K_Instagram_Icon_%28Black%29.svg/960px-CIS-A2K_Instagram_Icon_%28Black%29.svg.png" alt="Tanish" width="30" height="30"></img></a>
+        </div> 
+
     );
 }
